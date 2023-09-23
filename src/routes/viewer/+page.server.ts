5044f2e4141fad4d8fd772860b47f5dcf95b65e5
Isaac Dugan <isaacdugan@Isaacs-MacBook-Pro.local>
2023-09-23 17:07:06 -0400

    location and webcam working

---
+++ src/routes/viewer/+page.server.ts
@@ -0,0 +1,26 @@
+import type {Actions} from '@sveltejs/kit';
+import type { PageServerLoad } from '../$types';
+
+let status: number = 0
+export const actions: Actions = {
+    processImage: async({request})=>{
+        const data = await request.formData();
+        if(data.get('clip')){
+            status=200
+        }
+        else{
+            status=400
+        }
+        return {
+            response: status
+          }
+    }
+} satisfies Actions;
+
+
+
+export const load = (async () => {
+    return {
+        response: status
+    }
+}) satisfies PageServerLoad;
