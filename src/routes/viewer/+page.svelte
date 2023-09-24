4540d8a85cd0b952814fddd6309ec5c82f3ec7f2
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2023-09-24 11:38:47 -0400

    recorder

--- src/routes/viewer/+page.svelte
+++ src/routes/viewer/+page.svelte
@@ -60,7 +60,12 @@
 >
 	<div id="container">
 		<!-- svelte-ignore a11y-media-has-caption -->
-		<video autoplay id="videoElement" bind:this={video} />
+		<video
+			class="mt-32 border-[#f1d592] border-8 rounded-xl"
+			autoplay
+			id="videoElement"
+			bind:this={video}
+		/>
 	</div>
 	<canvas hidden id="canvas" width="640" height="480" bind:this={canvas} />
 
@@ -70,7 +75,7 @@
 	<button
 		on:click|preventDefault={flipRecording}
 		on:click|preventDefault={clipLoop}
-		class="flex justify-center m-auto text-4xl rounded-lg absolute inset-x-0 bottom-20 6xl bg-[#f1d592] drop-shadow-lg hover:bg-[#e6c984] py-2 px-4"
+		class="flex justify-center m-auto text-1xl rounded-lg absolute inset-x-0 bottom-20 6xl bg-[#f1d592] drop-shadow-lg hover:bg-[#e6c984] py-2 px-4 w-[10%]"
 		>Start Recording</button
 	>
 	<button
@@ -79,14 +84,14 @@
 		id="submit">submit</button
 	>
 </form>
-<div>{data.response}</div>
+
+<!-- <div>{data.response}</div> -->
 
 <style>
 	#container {
 		margin: 0px auto;
 		width: 700px;
 		height: 419px;
-		border: 10px #333 solid;
 	}
 	#videoElement {
 		width: 700px;
