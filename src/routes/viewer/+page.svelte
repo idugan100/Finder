625b63d1c80d3e7cc632b58ed39bee9b576c360d
Isaac Dugan <isaacdugan@Isaacs-MacBook-Pro.local>
2023-09-23 23:29:28 -0400

    making all buttons show

--- src/routes/viewer/+page.svelte
+++ src/routes/viewer/+page.svelte
@@ -60,10 +60,10 @@
         <canvas id="canvas" width="640" height="480"  bind:this="{canvas}"></canvas>
             <input hidden type="file" name="clip" id="" bind:this="{file_input}">
             <input hidden type="text" name="location" bind:this="{location}" id="">
-    <button class="flex justify-center m-auto text-4xl rounded-lg absolute inset-x-0 bottom-20 6xl bg-[#f1d592] drop-shadow-lg hover:bg-[#e6c984] py-2 px-4 " on:click|preventDefault={flipRecording} on:click|preventDefault={clipLoop}>Toggle Recording</button>
+    <button  on:click|preventDefault={flipRecording} on:click|preventDefault={clipLoop}>Toggle Recording</button>
     <button class="flex justify-center m-auto text-4xl rounded-lg absolute inset-x-0 bottom-20 6xl bg-[#f1d592] drop-shadow-lg hover:bg-[#e6c984] py-2 px-4 " type="submit">submit</button>
 </form>
-<button on:click={snapShot}>snapshot</button>
+<button  on:click={snapShot}>snapshot</button>
 <div>{data.response}</div>
 
 
