12d7632dcf6bc153cf3194eb862b1d5709b2c3b5
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2023-09-24 02:34:10 -0400

    admin backend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <nav class="flex gap-6 bg-[#f1d592] h-30 drop-shadow-lg">
-	<a class="text-xl hover:bg-[#e6c984] rounded pl-5" href="/">Home</a>
+	<a class="text-xl rounded pl-5" href="/">Finder</a>
+	<a class="text-xl hover:bg-[#e6c984] rounded" href="/admin">Admin</a>
 	<a class="text-xl hover:bg-[#e6c984] rounded" href="/viewer">View</a>
 	<div class="ml-auto">
 		<button class="text-xl hover:hover:bg-[#e6c984] rounded pr-5">Sign out</button>
