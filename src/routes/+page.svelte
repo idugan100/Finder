4311a5565ed2b4aa29e7fd8c7126da720ebbd084
thejohnmeyers <jmeyers5@gulls.salisbury.edu>
2023-09-24 01:12:34 -0400

    admin styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
 			  </div>
 			  <div class="flex items-center justify-center text-center">
 				<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-				  Sign In
+					<a href="/admin">Sign In</a>
 				</button>
 				</div>
 				</form>
