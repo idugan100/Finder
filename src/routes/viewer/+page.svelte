232f49f469943d2e3031d14f5de76db124a03a37
thejohnmeyers <jmeyers5@gulls.salisbury.edu>
2023-09-23 21:32:23 -0400

    styling for viewer

--- src/routes/viewer/+page.svelte
+++ src/routes/viewer/+page.svelte
@@ -56,19 +56,19 @@
         </div>
             <input hidden type="file" name="clip" id="">
             <input hidden type="text" name="location" bind:this="{location}" id="">
-    <button on:click|preventDefault={flipRecording} on:click|preventDefault={clipLoop}>record</button>
+    <button class="flex justify-center m-auto text-4xl rounded-lg absolute inset-x-0 bottom-20 6xl bg-[#f1d592] drop-shadow-lg hover:bg-[#e6c984] py-2 px-4 " on:click|preventDefault={flipRecording} on:click|preventDefault={clipLoop}>Toggle Recording</button>
 </form>
 
 <style>
     #container {
         margin: 0px auto;
-        width: 500px;
-        height: 375px;
+        width: 700px;
+        height: 419px;
         border: 10px #333 solid;
     }
     #videoElement {
-        width: 500px;
-        height: 375px;
+        width: 700px;
+        height: 400px;
         background-color: #666;
     }
     </style>
